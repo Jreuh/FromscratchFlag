b95c6d20418e2c0547b960c97263faf085afc361
J-R <jrd@weareled.fr>
2023-06-02 10:29:35 +0200

    tutoFinished

---
+++ src/components/Logo.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Logo() {
+  return (
+    <div className="logo">
+      <img src="./logo192.png" alt="Logo react" />
+      <h3>Fun With Flag</h3>
+    </div>
+  );
+}
---
+++ src/screens/About.js
@@ -0,0 +1,48 @@
+import React from "react";
+import Navigation from "../components/Navigation";
+import Logo from "../components/Logo";
+
+export default function About() {
+  return (
+    <div>
+      <Logo />
+      <Navigation />
+      <h1>A propos</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sed autem
+        similique! Itaque facere consequuntur quisquam minus tenetur repellat
+        libero asperiores nostrum eius accusantium tempora quia molestias, quae
+        eaque dolores. Iure perspiciatis, blanditiis quo reprehenderit quaerat
+        voluptates quisquam tempora. Iste dolorem sunt consectetur a possimus
+        nobis saepe, aliquam illum minima, voluptas qui non vitae, quae
+        molestiae pariatur soluta ullam odio? Voluptas ad laudantium libero quos
+        rerum itaque, harum accusantium corporis odio obcaecati distinctio
+        doloribus deleniti quis recusandae modi eum incidunt. Consectetur, magni
+        praesentium. Dolores consectetur, temporibus nisi delectus repudiandae
+        ducimus. Autem assumenda nostrum sint cum repellat ratione, doloremque
+        cumque eos, incidunt dolorum nam alias sit provident earum nisi ipsum
+        accusantium, aut quam molestias odit. Expedita veritatis dolorem
+        accusamus possimus commodi. Inventore omnis quae eos facere, explicabo
+        pariatur hic magni dicta possimus culpa ea dignissimos dolores
+        repudiandae id harum, qui ipsa, ullam repellendus praesentium velit
+        assumenda aspernatur porro temporibus laborum. Dolore! Accusantium odit
+        ea a esse vel qui ex culpa nemo corrupti, commodi labore reiciendis hic
+        sequi impedit? Possimus, officia minus. Facilis, architecto! Nesciunt,
+        laboriosam expedita accusamus vitae necessitatibus asperiores quas.
+        Impedit consequatur, expedita ducimus ipsa libero in dolorum eaque
+        quaerat voluptates nihil nam quidem blanditiis saepe facere possimus
+        voluptas laboriosam quasi! Est laboriosam at id recusandae nemo modi
+        voluptas nobis? Molestias dolore facere blanditiis! Natus magni amet
+        ratione, repellendus totam ex distinctio qui, pariatur similique vel in,
+        quo nulla nesciunt atque laboriosam eveniet saepe debitis. Rem optio
+        ipsam reprehenderit reiciendis? Veritatis molestias dolore modi nemo
+        aliquam nostrum quo! Porro id hic magnam fugiat temporibus quis vero
+        culpa accusantium quam mollitia provident corporis neque vitae, tenetur
+        blanditiis quibusdam consequatur nemo in? Aperiam odio incidunt enim
+        quod amet optio laudantium mollitia id explicabo qui soluta et vero
+        magni voluptas dolores maxime, ducimus nihil nisi corrupti atque sed
+        itaque harum! At, veniam ipsa?
+      </p>
+    </div>
+  );
+}
